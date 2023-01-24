8f3bd8dc4475194f56de3d379ef989cf60dc89a3
amartyamukherjee <am4521ty@gmail.com>
2023-01-24 12:28:34 -0500

    Updated HJBPPO docstring

--- stable_baselines3/hjbppo/hjbppo.py
+++ stable_baselines3/hjbppo/hjbppo.py
@@ -18,13 +18,6 @@ class HJBPPO(OnPolicyAlgorithm):
     """
     Hamilton Jacobi Bellman Proximal Policy Optimization algorithm (HJBPPO) (clip version)
 
-    Paper: https://arxiv.org/abs/1707.06347
-    Code: This implementation borrows code from OpenAI Spinning Up (https://github.com/openai/spinningup/)
-    https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail and
-    Stable Baselines (PPO2 from https://github.com/hill-a/stable-baselines)
-
-    Introduction to PPO: https://spinningup.openai.com/en/latest/algorithms/ppo.html
-
     :param policy: The policy model to use (MlpPolicy, CnnPolicy, ...)
     :param env: The environment to learn from (if registered in Gym, can be str)
     :param learning_rate: The learning rate, it can be a function
